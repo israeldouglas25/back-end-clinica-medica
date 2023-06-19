332e8d1ee76e3a8c110949501502c97d491e6f0f
Israel <isreldouglas25@gmail.com>
2023-06-19 20:53:36 -0300

    Incluindo a documentação por springdoc-openapi e anotando as classes com @SecurityRequirement para utilização do token

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
 import med.voll.api.domain.consulta.DadosCancelamentoConsulta;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/consultas")
+@SecurityRequirement(name = "bearer-key")
 public class ConsultaController {
 
     @Autowired
